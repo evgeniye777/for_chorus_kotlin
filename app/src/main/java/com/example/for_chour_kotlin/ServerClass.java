2bafa1396a8c49b67720a339fa4c4125feb7f9f5
Guzenko_EA <guzenko_ea@kamaz.ru>
2025-01-20 19:16:48 +0300

    next4

--- app/src/main/java/com/example/for_chour_kotlin/ServerClass.java
+++ app/src/main/java/com/example/for_chour_kotlin/ServerClass.java
@@ -14,6 +14,7 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ServerClass {
@@ -37,6 +38,7 @@ vivod(response,context);
         }){
             @Override
             protected Map<String,String> getParams(){
+                //"INSERT INTO account (login,password,email,name,birthday) VALUES ('$login','$password','$email','$name','$birthday')"
                 Map<String, String>  params = new HashMap<String, String>();
                 params.put("WelcomeNewYear", "2025");
                 return params;}
@@ -47,6 +49,10 @@ vivod(response,context);
                 return params;}};
         requestQueue.add(stringRequest);
         return answer[0];
+    }
+    private String generateRequestINSERT(String date, List<String> list) {
+        String request="INSERT INTO app_st_persons_chorus63 (date,c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20,c21,c22,c23,c24,c25,c26,c27,c28,c29,c30,c31,c32,c33,c34,c35,c36,c37,c38,c39,c40,c41,c42,c43,c44,c45,c46,c47,c48,c49,c50) VALUES (";
+        return request;
     }
     public void vivod(String str,Context context) {
         Toast.makeText(context,"Вы выбрали "
